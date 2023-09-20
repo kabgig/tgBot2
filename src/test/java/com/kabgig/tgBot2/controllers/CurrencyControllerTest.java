403857a788e0332a4a92f9c3c59082337151ad21
Ansar Ismagilov <ansar89kab@bk.ru>
2023-09-20 18:13:59 +0800

    in the process 3

---
+++ src/test/java/com/kabgig/tgBot2/controllers/CurrencyControllerTest.java
@@ -0,0 +1,25 @@
+package com.kabgig.tgBot2.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CurrencyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void testWhenAskAboutAllCurrencies() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/getCurrencies"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
