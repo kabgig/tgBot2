2ee8f4d6cde82941c33be1ac95be825aa215bfaf
Ansar Ismagilov <ansar89kab@bk.ru>
2023-09-21 17:47:08 +0800

    NamedParameterJdbcTemplate

--- src/main/java/com/kabgig/tgBot2/repository/StatsRepository.java
+++ src/main/java/com/kabgig/tgBot2/repository/StatsRepository.java
@@ -2,18 +2,33 @@ package com.kabgig.tgBot2.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
 public class StatsRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     public int getCountOfIncomesThatGreaterThan(BigDecimal amount){
-        return jdbcTemplate.queryForObject(
-                "SELECT count(*) FROM incomes WHERE income > ?",
-                Integer.class,
-                amount);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("amount", amount);
+        return namedParameterJdbcTemplate.queryForObject(
+                "SELECT count(*) FROM incomes WHERE income > :amount",
+                parameters, new StatsRowMapper());
     }
+    private static final class StatsRowMapper implements RowMapper<Integer>{
+        @Override
+        public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return rs.getInt("COUNT(*)");
+        }
+    }
+
 }
