45c768ac8dff5f803dcc971a40d5b813ef555cbb
Ansar Ismagilov <ansar89kab@bk.ru>
2023-10-02 22:01:26 +0800

    added H2 for testing and populated with testing data

--- src/main/java/com/kabgig/tgBot2/repository/StatsRepository.java
+++ src/main/java/com/kabgig/tgBot2/repository/StatsRepository.java
@@ -59,5 +59,4 @@ public class StatsRepository {
             return rs.getInt("COUNT(*)");
         }
     }
-
 }
--- src/test/java/com/kabgig/tgBot2/service/StatsServiceTest.java
+++ src/test/java/com/kabgig/tgBot2/service/StatsServiceTest.java
@@ -1,5 +1,6 @@
 package com.kabgig.tgBot2.service;
 
+import com.kabgig.tgBot2.repository.IncomeRepository;
 import com.kabgig.tgBot2.repository.StatsRepository;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -7,22 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.properties")
-@ActiveProfiles("test")
-@Sql(scripts = "classpath:data-test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "classpath:data-after-tests.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//@ActiveProfiles("test")
+//@Sql(scripts = "classpath:data-test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(scripts = "classpath:data-after-tests.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class StatsServiceTest {
 
     private final StatsRepository statsRepository;
-
     @Autowired
-    StatsServiceTest(StatsRepository statsRepository) {
+    StatsServiceTest(StatsRepository statsRepository, IncomeRepository incomeRepository) {
         this.statsRepository = statsRepository;
     }
 
@@ -35,6 +34,10 @@ class StatsServiceTest {
             if (i <= 20) isOkay = false;
         }
         Assert.assertTrue(isOkay);
-
+    }
+    @Test
+    void getCountOfSpendingsThatGreaterThanTest(){
+        int i = statsRepository.getCountOfSpendingsThatGreaterThan(new BigDecimal(100));
+        Assert.assertEquals(5,  i);
     }
 }
